1ff0f35758453d7408b734e00ee87a81bbbb65f0
gxguinho <gxguinho@gmail.com>
2024-03-02 16:29:38 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; } = String.Empty;
+
+        public ICollection<Product> Products { get; private set; } = [];
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(string name) 
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            ValidateDomain(name);
+
+            Id = id;
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid Category.Name is Required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 charecters");
+
+            Name = name;
+        }
+    }
+}
