1346625a1e9c35df7c7dc63fc8c55d39ee1da414
gxguinho <gxguinho@gmail.com>
2024-03-02 19:04:52 -0300

    add infra layer

--- src/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ src/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -12,8 +12,8 @@ namespace CleanArchMvc.Domain.Interfaces
         Task<IEnumerable<Product>> GetProducts();
         Task<Product> GetById(int? id);
         Task<Product> GetProductCategory(int? id);
-        Task<Product> Create(Product category);
-        Task<Product> Update(Product category);
-        Task<Product> Remove(Product category);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
     }
 }
---
+++ src/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _productContext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategory(int? id)
+        {
+            return await _productContext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
